ce667b0336da713a3467e9c74d78d94c840951c6
Yanluis Fermin <tfermin1827@hotmail.com>
2019-07-12 20:02:15 -0700

    Added GameModeChange event handler

--- src/main/java/me/jackint0sh/timedfly/TimedFly.java
+++ src/main/java/me/jackint0sh/timedfly/TimedFly.java
@@ -6,10 +6,7 @@ import me.jackint0sh.timedfly.commands.TFly;
 import me.jackint0sh.timedfly.commands.TabCompleter;
 import me.jackint0sh.timedfly.flygui.FlyItem;
 import me.jackint0sh.timedfly.flygui.FlyInventory;
-import me.jackint0sh.timedfly.listeners.AttackListener;
-import me.jackint0sh.timedfly.listeners.ChatListener;
-import me.jackint0sh.timedfly.listeners.InventoryListener;
-import me.jackint0sh.timedfly.listeners.PlayerDamageListener;
+import me.jackint0sh.timedfly.listeners.*;
 import me.jackint0sh.timedfly.managers.PlayerManager;
 import me.jackint0sh.timedfly.managers.TimerManager;
 import me.jackint0sh.timedfly.utilities.Config;
@@ -71,6 +68,7 @@ public final class TimedFly extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerDamageListener(), this);
         Bukkit.getPluginManager().registerEvents(new AttackListener(), this);
+        Bukkit.getPluginManager().registerEvents(new GameModeListener(), this);
 
         MessageUtil.sendConsoleMessage("&cEvent listeners successfully loaded!");
     }
