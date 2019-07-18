73b6765cc9134e4cf41fe66a8bbf5b7cda332810
Yanluis Fermin <tfermin1827@hotmail.com>
2019-07-17 23:55:59 -0700

    Added missing permission enums

--- src/main/java/me/jackint0sh/timedfly/utilities/Permissions.java
+++ src/main/java/me/jackint0sh/timedfly/utilities/Permissions.java
@@ -2,7 +2,18 @@ package me.jackint0sh.timedfly.utilities;
 
 public enum  Permissions {
 
-    ADMIN("timedfly.admin", "Get access to everything.");
+    ADMIN("timedfly.admin", "Get access to everything."),
+    GET_UPDATE("timedfly.getupdate", "Get an update notification if available."),
+    BYPASS_ATTACK("timedfly.bypass.attack", "Bypass restriction of toggle fly off for entering attack mode."),
+    BYPASS_LIMIT("timedfly.bypass.attack", "Bypass the limit of time bought."),
+    FLY_SET("timedfly.fly.set", "Set fly time to your self."),
+    FLY_SET_OTHER("timedfly.fly.set.other", "Set fly time to another player."),
+    FLY_ADD("timedfly.fly.add.other", "Add fly time to another player."),
+    FLY_ADD_OTHER("timedfly.fly.add.other", "Add fly time to another player."),
+    FLY_TOGGLE("timedfly.fly.toggle", "Resume or pause your fly time."),
+    FLY_TOGGLE_OTHER("timedfly.fly.toggle.other", "Resume or pause another player's fly time."),
+    SKIP_STORE("timedfly.fly.skipstore", "Toggle your flight on/off instead of opening the store."),
+    SKIP_STORE_OTHER("timedfly.fly.skipstore.other", "Toggle another player's flight on/off instead of opening the store.");
 
     private String permission;
     private String description;
