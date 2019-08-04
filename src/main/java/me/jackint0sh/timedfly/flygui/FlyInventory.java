43ca9638fc9552b1df5f25b9926f61864c2dbf1f
Yanluis Fermin <tfermin1827@hotmail.com>
2019-08-03 21:00:01 -0400

    Item dropped on floor if no space on player's inventory

--- src/main/java/me/jackint0sh/timedfly/flygui/FlyInventory.java
+++ src/main/java/me/jackint0sh/timedfly/flygui/FlyInventory.java
@@ -3,10 +3,12 @@ package me.jackint0sh.timedfly.flygui;
 import me.jackint0sh.timedfly.utilities.MessageUtil;
 import me.jackint0sh.timedfly.versions.ServerVersion;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -116,7 +118,11 @@ public class FlyInventory {
             Arrays.stream(this.getInventory().getContents())
                     .filter(Objects::nonNull)
                     .filter(itemStack -> !ServerVersion.getSupportedVersion().hasTag(itemStack, "customitem"))
-                    .forEach(((InventoryCloseEvent) e).getPlayer().getInventory()::addItem);
+                    .forEach(item -> {
+                        Player player = (Player) ((InventoryCloseEvent) e).getPlayer();
+                        Map<Integer, ItemStack> contents = player.getInventory().addItem(item);
+                        if (!contents.isEmpty()) player.getWorld().dropItem(player.getLocation(), item);
+                    });
             if (this.closeConsumer != null){
                 this.closeConsumer.andThen(event -> this.clearInventory()).accept((InventoryCloseEvent) e);
             }
