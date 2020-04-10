f0c14f195b1064f6daface3f2b605e68f7afa5cd
Jack <tfermin1827@hotmail.com>
2020-04-10 10:34:59 -0400

    Started TimedFly premium version

---
+++ src/main/java/me/jackscode/timedfly/TimedFly.java
@@ -0,0 +1,17 @@
+package me.jackscode.timedfly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TimedFly extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
