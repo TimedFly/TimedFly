5ea445f9bc0b0520ab5a1f46b495e4ab5860273e
Jack <tfermin1827@hotmail.com>
2020-04-10 13:06:42 -0400

    Add: Currency interface

---
+++ src/main/java/me/jackscode/timedfly/api/TimedFlyCurrency.java
@@ -0,0 +1,15 @@
+package me.jackscode.timedfly.api;
+
+import org.bukkit.entity.Player;
+
+public interface TimedFlyCurrency {
+    String name();
+
+    boolean withdraw(Player player, int amount);
+
+    boolean deposit(Player player, int amount);
+
+    boolean has(Player player, int amount);
+
+    int balance(Player player);
+}
