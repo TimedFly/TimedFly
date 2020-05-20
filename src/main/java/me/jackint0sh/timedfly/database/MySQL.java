fd04649d3c8c8e93069bbf38e7b2e260cd030d04
McDaddyTalk <kbtalkin@gmail.com>
2020-05-20 16:03:01 -0400

    #44 added config setting for useSSL

--- src/main/java/me/jackint0sh/timedfly/database/MySQL.java
+++ src/main/java/me/jackint0sh/timedfly/database/MySQL.java
@@ -13,9 +13,29 @@ public class MySQL extends SQL {
         super(plugin);
     }
 
-    @Override
-    public AsyncDatabase connect(String host, int port, String database, String user, String password, Callback<String> callback) {
+    public AsyncDatabase connect(String host, int port, String database, String user, String password, Boolean useSSL, Callback<String> callback) {
         try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            callback.handle(e, null);
+            return this;
+        }
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database
+                    + "?autoReconnect=true&useSSL=" + useSSL, user, password);
+            callback.handle(null, null);
+        } catch (SQLException e) {
+            callback.handle(e, null);
+        }
+        return this;
+    }
+
+    // Legacy support or for any reason useSSL is not sent
+	@Override
+	public AsyncDatabase connect(String host, int port, String database, String user, String password,
+			Callback<String> callback) {
+		try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             callback.handle(e, null);
@@ -30,6 +50,6 @@ public class MySQL extends SQL {
             callback.handle(e, null);
         }
         return this;
-    }
+	}
 
 }
