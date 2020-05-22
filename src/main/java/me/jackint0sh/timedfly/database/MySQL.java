e52465357babe347df4aa4c0bde8793a6d1764a3
McDaddyTalk <kbtalkin@gmail.com>
2020-05-21 20:52:45 -0400

    #44 update made at suggestion of Jacxk

--- src/main/java/me/jackint0sh/timedfly/database/MySQL.java
+++ src/main/java/me/jackint0sh/timedfly/database/MySQL.java
@@ -30,26 +30,10 @@ public class MySQL extends SQL {
         }
         return this;
     }
-
-    // Legacy support or for any reason useSSL is not sent
-	@Override
-	public AsyncDatabase connect(String host, int port, String database, String user, String password,
-			Callback<String> callback) {
-		try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            callback.handle(e, null);
-            return this;
-        }
-
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database
-                    + "?autoReconnect=true&useSSL=false", user, password);
-            callback.handle(null, null);
-        } catch (SQLException e) {
-            callback.handle(e, null);
-        }
-        return this;
-	}
-
+    
+    // Needed to satisfy abstraction of AsyncDatabase via SQL extension
+    // Or make MySQL abstract...   
+    public AsyncDatabase connect(String host, int port, String database, String user, String password, Callback<String> callback) {
+     	return connect(host, port, database, user, password, false, callback);
+    }
 }
